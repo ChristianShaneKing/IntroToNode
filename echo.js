7f076a0018259ed4ae17efe6475158f32cbea19b
Christian King <christianshaneking@gmail.com>
2019-08-03 00:30:39 +0000

    add echo.js file

---
+++ echo.js
@@ -0,0 +1,10 @@
+// I really tried to come up with a function on my own and I was pretty close but still needed to refer to colt to get the solution! Next time I think I can do it from memory...
+
+function echo(str, num){
+	for(var i = 0; i <= num; i++){
+		console.log(str);
+	}
+}
+
+echo("Echo!!!", 10)
+echo("Tater Tots", 3)
